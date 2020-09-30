b6c3ef36d784bd03c78cf876ba8cd8e96e6df1f9
libingbing-li <2998314861@qq.com>
2020-09-30 09:50:28 +0800

    Updates

---
+++ precache-manifest.8f314da4c6e207a750153537583d69e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db96c6eaa292fdf254c6e11c13442e4",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "0c09337e0d157b27672e",
+    "url": "/demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a2beead998f941c437d2",
+    "url": "/demo/static/js/2.ac5db91e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo/static/js/2.ac5db91e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c09337e0d157b27672e",
+    "url": "/demo/static/js/main.6a42b4a7.chunk.js"
+  },
+  {
+    "revision": "cc04ac744fdb9496e053",
+    "url": "/demo/static/js/runtime-main.f6abbe3b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
